ab17fe4807ded1217abf7655b537a3ba5e2e8cd2
DogmaDragon <103123951+DogmaDragon@users.noreply.github.com>
2024-07-12 03:55:07 +0300

    Fix IMPORTED_CSS link
    Also update domaink for FansDB match.

--- src/header/StashDB Draft Image Pasting.user.js
+++ src/header/StashDB Draft Image Pasting.user.js
@@ -5,8 +5,8 @@
 // @version     0.1.0
 // @author      7dJx1qP
 // @match       https://stashdb.org/*
-// @match       https://fansdb.xyz/*
-// @resource    IMPORTED_CSS https://raw.githubusercontent.com/7dJx1qP/stashdb-userscripts/dist/public/scene.css
+// @match       https://fansdb.cc/*
+// @resource    IMPORTED_CSS https://raw.githubusercontent.com/elkorol/stashdb-userscripts/master/dist/public/scene.css
 // @grant       unsafeWindow
 // @grant       GM_setClipboard
 // @grant       GM_getResourceText
